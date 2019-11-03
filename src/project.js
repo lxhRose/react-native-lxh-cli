64f8cbd95147bdea6d30fd2f6da86fb6b4fb3b26
lvxinghai <lvxinghai@mecby.com>
2019-11-03 18:57:55 +0800

    指定依赖版本。

--- src/project.js
+++ src/project.js
@@ -63,73 +63,99 @@ Project.prototype.inquire = function () {
 };
 
 Project.prototype.generate = function () {
-  const { projectName, version } = this.config;
+  this.initReactNative();
+}
 
-  // 1、用 React-Native 脚手架创建基本项目，以保证项目的依赖版本为最新
+/**
+ * 1、用 React-Native 脚手架初始化项目
+ */
+Project.prototype.initReactNative = function () {
+  const { projectName, version } = this.config;
   console.log();
-  const RNINIT = `react-native init ${projectName} ${version ? `--version ${version}` : ''}`;
+  let _version = typeof version === 'string' ? `--version ${version}` : '';
+  const RNINIT = `react-native init ${projectName} ${_version}`;
   const RNinitSpinner = ora(`初始化项目 ${green.bold(RNINIT)}, 大概需要几分钟的时间，请耐心等待...`);
   RNinitSpinner.start();
   exec(RNINIT, (error, stdout, stderr) => {
     if (error) {
       RNinitSpinner.color = 'red';
-      RNinitSpinner.fail(red('初始化项目失败！'));
+      RNinitSpinner.fail(red('初始化项目失败！请确保您已经全局安装过react-native-cli'));
       console.log(error);
     } else {
       RNinitSpinner.color = 'green';
       RNinitSpinner.succeed('初始化项目成功！');
       console.log(`${stderr}${stdout}`);
-
-      // 2、下载自己git上存放的模板和必要文件
-      console.log();
-      const downloadSpinner = ora('正在下载模板，请稍等...');
-      downloadSpinner.start();
-      const projectPath = path.join(process.cwd(), projectName);
-      const downloadPath = path.join(projectPath, '__download__');
-      download(TEMPLATE_GIT_REPO, downloadPath, { clone: true }, (err) => {
-        if (err) {
-          downloadSpinner.color = 'red';
-          downloadSpinner.fail(err.message);
-          return;
-        }
-        downloadSpinner.color = 'green';
-        downloadSpinner.succeed('下载模板成功！');
-
-        // 复制文件
-        console.log();
-        const copyFiles = getDirFileName(downloadPath);
-
-        copyFiles.forEach((file) => {
-          fse.copySync(path.join(downloadPath, file), path.join(projectPath, file));
-          console.log(`${green('√')}${grey(` 创建: ${projectName}/${file}`)}`);
-        });
-
-        fse.remove(downloadPath);
-        process.chdir(projectPath);
-
-        // 3、安装依赖
-        console.log();
-        const INSTALL = 'yarn add axios react-native-gesture-handler react-native-reanimated react-native-screens react-native-ui-lvxinghai react-native-webview react-navigation react-redux redux redux-logger redux-thunk @react-native-community/async-storage';
-        const installSpinner = ora(`安装项目依赖 ${green.bold(INSTALL)}, 请稍后...`);
-        installSpinner.start();
-        exec(INSTALL, (error, stdout, stderr) => {
-          if (error) {
-            installSpinner.color = 'red';
-            installSpinner.fail(red('安装项目依赖失败，请自行重新安装！'));
-            console.log(error);
-          } else {
-            installSpinner.color = 'green';
-            installSpinner.succeed('安装依赖成功！');
-            console.log(`${stderr}${stdout}`);
-            console.log();
-            console.log(`${green('√')}${green(' 创建项目成功！')}`);
-            console.log();
-            console.log(`${green('√')}${green(' 开始愉快地搬砖吧！')}`);
-          }
-        }) // 3--end
-      }) // 2--end
+      this.downloadTemplate(projectName);
     }
-  }) //1--end
+  });
+}
+
+/**
+ * 2、下载自己git上存放的模板和必要文件
+ */
+Project.prototype.downloadTemplate = function (projectName) {
+  console.log();
+  const downloadSpinner = ora('正在下载模板，请稍等...');
+  downloadSpinner.start();
+  const projectPath = path.join(process.cwd(), projectName);
+  const downloadPath = path.join(projectPath, '__download__');
+  download(TEMPLATE_GIT_REPO, downloadPath, { clone: true }, (err) => {
+    if (err) {
+      downloadSpinner.color = 'red';
+      downloadSpinner.fail(err.message);
+      return;
+    }
+    downloadSpinner.color = 'green';
+    downloadSpinner.succeed('下载模板成功！');
+
+    // 复制文件
+    console.log();
+    const copyFiles = getDirFileName(downloadPath);
+
+    copyFiles.forEach((file) => {
+      fse.copySync(path.join(downloadPath, file), path.join(projectPath, file));
+      console.log(`${green('√')}${grey(` 创建: ${projectName}/${file}`)}`);
+    });
+
+    fse.remove(downloadPath);
+    process.chdir(projectPath);
+    this.installDependency();
+  });
+}
+
+/**
+ * 3、安装依赖
+ */
+Project.prototype.installDependency = function () {
+  this.dependencyFunc('yarn add', (yarnSpinner, error) => {
+    yarnSpinner.fail(red('安装项目依赖失败，将改用 npm install 安装！'));
+    console.log(error);
+    this.dependencyFunc('npm install', (yarnSpinner, error) => {
+      yarnSpinner.fail(red('安装项目依赖失败，请自行重新安装！'));
+      console.log(error);
+    });
+  });
+}
+
+Project.prototype.dependencyFunc = function (type, errorFunc) {
+  console.log();
+  const INSTALL = 'axios@^0.19.0 react-native-gesture-handler@^1.3.0 react-native-linear-gradient@^2.5.4 react-native-ui-lvxinghai react-native-webview@^5.12.1 react-navigation@^3.11.0 react-redux@^7.1.0 redux@^4.0.1 redux-logger@^3.0.6 redux-thunk@^2.3.0 @react-native-community/async-storage@^1.5.0';
+  const yarnSpinner = ora(`安装项目依赖 ${green.bold(`${type} ${INSTALL}`)}, 请稍后...`);
+  yarnSpinner.start();
+  exec(`${type} ${INSTALL}`, (error, stdout, stderr) => {
+    if (error) {
+      yarnSpinner.color = 'red';
+      errorFunc(yarnSpinner, error);
+    } else {
+      yarnSpinner.color = 'green';
+      yarnSpinner.succeed('安装依赖成功！');
+      console.log(`${stderr}${stdout}`);
+      console.log();
+      console.log(`${green('√')}${green(' 项目创建成功！')}`);
+      console.log();
+      console.log(`${green('√')}${green(' 开始愉快地搬砖吧！')}`);
+    }
+  });
 }
 
 module.exports = Project;
